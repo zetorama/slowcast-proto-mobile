670f115e1836c39ae54bc495237c244605683dd5
Pavel Zet <zetorama@gmail.com>
2019-05-29 00:02:43 -0700

    fix(app): render nothing, when app is in background

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { AppState } from 'react-native'
 import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react'
 
@@ -10,6 +11,16 @@ const persistor = getPersistor()
 
 
 export function App() {
+  const [appState, setAppState] = useState(AppState.currentState)
+
+  useEffect(() => {
+    AppState.addEventListener('change', setAppState)
+    return () => AppState.removeEventListener('change', setAppState)
+  })
+
+  // Just render nothing, when app is in background
+  if (appState !== 'active') return null
+
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
